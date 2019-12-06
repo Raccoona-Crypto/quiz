9d0083eccb73169f10f00e6df4dc85749d3b9cd2
Vladyslav Melnyk <v.v.melnyk1994@gmail.com>
2019-12-06 19:17:00 +0200

    add user agent globally

--- src/main/java/com/raccoona/configuration/ApplicationConfig.java
+++ src/main/java/com/raccoona/configuration/ApplicationConfig.java
@@ -8,8 +8,10 @@ import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.params.MainNetParams;
 import org.bitcoinj.params.TestNet3Params;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -17,7 +19,11 @@ public class ApplicationConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        ClientHttpRequestInterceptor interceptor = (request, body, execution) -> {
+            request.getHeaders().add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+            return execution.execute(request, body);
+        };
+        return new RestTemplateBuilder().additionalInterceptors(interceptor).build();
     }
 
     @Bean
--- src/main/java/com/raccoona/service/BlockbookService.java
+++ src/main/java/com/raccoona/service/BlockbookService.java
@@ -7,18 +7,16 @@ import com.raccoona.dto.UtxoDto;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Service
 public class BlockbookService {
@@ -39,14 +37,9 @@ public class BlockbookService {
         UriComponentsBuilder builder = UriComponentsBuilder
                 .fromUriString(url)
                 .queryParam("confirmed", true);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(APPLICATION_JSON));
-        headers.set("x-request-source", "desktop");
-        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-        HttpEntity request = new HttpEntity(headers);
         logger.info(String.format("Get UTXO for address %s and url %s", address, builder.toUriString()));
 
-        ResponseEntity<UtxoDto[]> responseEntity = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, request, UtxoDto[].class);
+        ResponseEntity<UtxoDto[]> responseEntity = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, null, UtxoDto[].class);
 
         logger.info(String.format("response: %s", responseEntity.getStatusCode()));
         Set<UtxoDto> result = Arrays.asList(responseEntity.getBody()).stream().collect(Collectors.toSet());
